05468df6ba249488da58157a7625465555c1d482
Mike Busch <mbusch@heroku.com>
2019-08-13 15:29:46 -0400

    add deploy field to logger

--- config/initializers/log.rb
+++ config/initializers/log.rb
@@ -1,3 +1,6 @@
-Pliny.default_context = {app: "telex"}
+Pliny.default_context = {
+  app: "telex",
+  deploy: Config.deployment
+}
 
 Pliny.log_scrubber = BlacklistHash.build(fields_to_scrub: Rollbar::Blanket.fields, scrub_urls: true)
